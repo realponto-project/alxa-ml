e2554786dc520041f23f3f7370c1160ed7ade608
jessiLeandro <jessi_leandro@hotmail.com>
2021-05-25 10:48:38 -0300

    chore: some update changes

--- src/Pages/Ads/Manager/index.js
+++ src/Pages/Ads/Manager/index.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { isNil, length, pathOr } from 'ramda'
 import { Form } from 'antd'
+import readXlsxFile from 'read-excel-file'
 
 import ManagerContainer from '../../../Containers/Ads/Manager'
 import { getAll, getCusmtomerById } from '../../../Services/Ads'
 import {
   getAllAccounts,
-  getLoaderAdsByMlAccountId
+  getLoaderAdsByMlAccountId,
+  updateAds
 } from '../../../Services/mercadoLibre'
 import { buildFormValuesCustomer } from '../../../utils/Specs/Customer'
+
+const schema = {
+  SKU: {
+    prop: 'sku',
+    type: String
+  },
+  PRICE: {
+    prop: 'price',
+    type: Number
+  }
+}
 
 const Manager = () => {
   const [accounts, setAccounts] = useState([])
@@ -30,7 +43,6 @@ const Manager = () => {
 
     try {
       const { data } = await getAll({ ...formValues, order, page, limit: 10 })
-      console.log(data)
       setSource(data.source)
       setTotal(data.total)
     } catch (error) {}
@@ -45,7 +57,7 @@ const Manager = () => {
     }
 
     setOrder(order ? [[columnKey, formatOrder[order]]] : [])
-    setPage(current)
+    setPage(order ? 1 : current)
   }
 
   const closeModalAdd = () => {
@@ -77,7 +89,9 @@ const Manager = () => {
     if (!mlAccountId) return
 
     getLoaderAdsByMlAccountId(mlAccountId).then((response) => {
-      console.log(response)
+      if (response.status === 200) {
+        setVisibleModalAdd(false)
+      }
     })
   }
 
@@ -91,6 +105,21 @@ const Manager = () => {
   const handleSubmitForm = (formData) => {
     setPage(1)
     setFormValues(formData)
+  }
+
+  const handleChangeUpload = (reference) => {
+    readXlsxFile(reference.current.files[0], { schema }).then(function ({
+      rows
+    }) {
+      const skuList = []
+      const priceList = []
+      rows.forEach(({ sku, price }) => {
+        skuList.push(sku)
+        priceList.push(price)
+      })
+
+      updateAds({ skuList, priceList })
+    })
   }
 
   useEffect(() => {
@@ -109,26 +138,27 @@ const Manager = () => {
 
   return (
     <ManagerContainer
-      formSearch={formSearch}
-      handleClearForm={handleClearForm}
-      handleSubmitForm={handleSubmitForm}
+      accounts={accounts}
       closeModalAdd={closeModalAdd}
       expand={expand}
       formAdd={formAdd}
+      formSearch={formSearch}
+      handleChangeAccount={(id) => setMlAccountId(id)}
+      handleChangeUpload={handleChangeUpload}
+      handleClearForm={handleClearForm}
       handleClickEdit={handleClickEdit}
       handleClickExpand={handleClickExpand}
-      modelTitle={isNil(id) ? 'Cadastro cliente' : 'Atualizar cliente'}
-      openModalAdd={() => setVisibleModalAdd(true)}
-      source={source}
-      visibleModalAdd={visibleModalAdd}
-      loading={loading}
-      onChangeTable={onChangeTable}
-      total={total}
-      page={page}
-      accounts={accounts}
+      handleSubmitForm={handleSubmitForm}
       handleSubmitSync={handleSubmitSync}
-      handleChangeAccount={(id) => setMlAccountId(id)}
+      loading={loading}
       mlAccountId={mlAccountId}
+      modelTitle={isNil(id) ? 'Cadastro cliente' : 'Atualizar cliente'}
+      onChangeTable={onChangeTable}
+      openModalAdd={() => setVisibleModalAdd(true)}
+      page={page}
+      source={source}
+      total={total}
+      visibleModalAdd={visibleModalAdd}
     />
   )
 }
